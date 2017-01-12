d073edb82db38e05ffa30567a1b2234ed249cc1f
Max Fowler <maxhfowler@gmail.com>
2017-01-12 03:49:44 -0500

    nginx conf

--- backend/hello_settings.py
+++ backend/hello_settings.py
@@ -1,8 +1,15 @@
 import os, json
 
+# are we local?
+LOCAL = os.environ.get('LOCAL')
+DEBUG = LOCAL
 
 # project path
-PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+# TODO: fix this so that it is the same for local and prod
+if LOCAL:
+    PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+else:
+    PROJECT_PATH = '/srv/hello_webapp'
 BACKEND_PATH = os.path.join(PROJECT_PATH, 'backend')
 print 'PROJECT_PATH: {}'.format(PROJECT_PATH)
 print 'BACKEND_PATH: {}'.format(BACKEND_PATH)
@@ -10,11 +17,6 @@ print 'BACKEND_PATH: {}'.format(BACKEND_PATH)
 # secrets dict
 SECRETS_PATH = os.path.join(BACKEND_PATH, 'devops/secret_files/secret.json')
 SECRETS_DICT = json.loads(open(SECRETS_PATH, "r").read())
-
-
-# are we local?
-LOCAL = os.environ.get('LOCAL')
-DEBUG = LOCAL
 
 
 # temporary settings below
